17431d3f9d8fd46b0c9a243d2b01616a71dce162
RomanThapliyal <romanthapliyal2@gmail.com>
2026-09-16 22:51:54 +0530

    fix: correct import in DatasetDAO from User to Dataset

--- src/dao/DatasetDAO.java
+++ src/dao/DatasetDAO.java
@@ -1,13 +1,13 @@
 package dao;
 
-import  java.sql.Connection;
-import  java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import  java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
-import model.User;
+import model.Dataset;
 import util.DBConnection;
 
 public class DatasetDAO {
